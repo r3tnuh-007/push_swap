cf57b4a8e8e69e218c5a03f5b6ec293af7cc9cec
r3tnuh-007 <anterofranciso@gmail.com>
2025-12-07 01:15:38 +0100

    Building things up

---
+++ srcs/utils/stack_init.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_init.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aluis <aluis@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/28 00:11:30 by aluis             #+#    #+#             */
+/*   Updated: 2025/12/06 23:34:30 by aluis            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/push_swap.h"
+
+/*
+ * Create the stack with the command line values
+ * Checks are embedded in the creation itslef
+ * 		~Duplicate values
+ * 		~Over|Underflow
+ * 		~Syntax errors
+ *
+ * 	🏁 Flag is useful cause if true, i have the argv in the HEAP to free
+ *
+*/
+void	stack_init(t_stack_node **a, char **argv, bool flag_argc_2)
+{
+	long	nbr;
+	int		i;
+
+	i = 0;
+	while (argv[i])
+	{
+		if (error_syntax(argv[i]))
+			error_free(a, argv, flag_argc_2);
+		nbr = ft_atol(argv[i]);
+		if (nbr > INT_MAX || nbr < INT_MIN)
+			error_free(a, argv, flag_argc_2);
+		if (error_repetition(*a, (int)nbr))
+			error_free(a, argv, flag_argc_2);
+		append_node(a, (int)nbr);
+		++i;
+	}
+	if (flag_argc_2)
+		free_matrix(argv);
+}
