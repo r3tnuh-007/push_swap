477f6653d19fe19773dc28b6bcee09268c282792
r3tnuh-007 <anterofranciso@gmail.com>
2025-12-07 15:56:06 +0100

    parse problems

--- srcs/main.c
+++ srcs/main.c
@@ -6,25 +6,27 @@
 /*   By: aluis <aluis@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 00:16:33 by aluis             #+#    #+#             */
-/*   Updated: 2025/12/07 12:21:45 by aluis            ###   ########.fr       */
+/*   Updated: 2025/12/07 15:29:18 by aluis            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
+#include <stdio.h>
 
 int	main(int argc, char **argv)
 {
 	t_stack_node	*a;
 	t_stack_node	*b;
+	char			**temp_argv;
 
 	a = NULL;
 	b = NULL;
 	if (argc == 1 || (argc == 2 && !argv[1][0]))
 		return (1);
-	argv = parser(argv, argc);
-//	for(int i = 0; i < argc; i++)
-//		printf("argv[%d]: %s\n", i, argv[i]);
-	stack_init(&a, argv + 1, 2 == argc);
+	temp_argv = parser(argv, argc);
+//	for(size_t i = 0; i < ft_strlen(argv); i++)
+//		printf("argv[%zu]: %s\n", i, argv[i]);
+	stack_init(&a, temp_argv + 1, true);
 	if (!stack_sorted(a))
 	{
 		if (stack_len(a) == 2)
--- srcs/utils/parser.c
+++ srcs/utils/parser.c
@@ -6,7 +6,7 @@
 /*   By: aluis <aluis@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 00:16:33 by aluis             #+#    #+#             */
-/*   Updated: 2025/12/07 12:36:50 by aluis            ###   ########.fr       */
+/*   Updated: 2025/12/07 15:09:15 by aluis            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,10 @@ char	**parser(char **argv, int argc)
 	int		i;
 
 	i = 0;
-	temp_argv = ft_split(argv[i ++], ' ' );
-	while(i < argc)
+	temp_argv = ft_split(argv[i ++], ' ');
+	while (i < argc)
 	{
-		temp_argv2 = ft_split(argv[i ++], ' ' );
+		temp_argv2 = ft_split(argv[i ++], ' ');
 		temp_argv = ft_strjoin(temp_argv, temp_argv2);
 	}
 	return (temp_argv);
